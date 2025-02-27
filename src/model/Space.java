50e5a7917454dda51451174a1b7f84ac0101dc26
João_A.11 <jarlpereira29@gmail.com>
2025-02-27 17:27:53 -0300

    Adiciona Space

---
+++ src/model/Space.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Space {
+
+    private Integer currentNumber;
+    private boolean fixed;
+
+    public Space(Integer currentNumber, boolean fixed) {
+        this.currentNumber = currentNumber;
+        this.fixed = fixed;
+    }
+
+    public int getCurrentNumber() {
+        return currentNumber;
+    }
+
+    public boolean setCurrentNumber(Integer currentNumber) {
+        if (isFixed()) {
+            return false;
+        }
+        this.currentNumber = currentNumber;
+        return true;
+    }
+
+    public boolean isFixed() {
+        return fixed;
+    }
+
+    public boolean clearNumber() {
+        if (isFixed()) {
+            return false;
+        }
+        currentNumber = null;
+        return true;
+    }
+}
